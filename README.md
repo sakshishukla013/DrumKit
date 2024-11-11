c4595c293e721bce3a738aab3ef0a6afbc40b60b
sakshishukla013 <sakshishukla013@gmail.com>
2024-11-11 12:39:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DrumKit_Js
+A DrumKit made using Javascript
+![Screenshot 2022-12-22 080141](https://user-images.githubusercontent.com/86218655/209042438-ec883453-7077-4d0b-b7cf-f7f3b706e052.png)
+Press the keyboard buttons or click the mouse to enjoy the sound of different instruments.
+Deployed:https://vaibhavdwivedi-321.github.io/DrumKit_Js/
---
+++ index.js
@@ -0,0 +1,70 @@
+var numberOfDrumButtons =  document.querySelectorAll(".drum").length;
+for  (var i = 0; i<numberOfDrumButtons; i++ ) {
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+//adding addEventListener and checking which key isbeing pressed
+var buttonInnerHTML = this.innerHTML;
+//passing the key pressed in makeosund
+makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+});
+}
+
+
+// adding addEventListener to detect which keyboard key is pressed
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+      break;
+
+     case "a":
+     var tom2 = new Audio("sounds/tom-2.mp3");
+     tom2.play();
+     break;
+
+     case "s":
+     var tom3 = new Audio("sounds/tom-3.mp3");
+     tom3.play();
+     break;
+
+     case "d":
+     var tom4 = new Audio("sounds/tom-4.mp3");
+     tom4.play();
+     break;
+
+     case "j":
+     var snare = new Audio("sounds/snare.mp3");
+     snare.play();
+     break;
+
+     case "k":
+     var crash = new Audio("sounds/crash.mp3");
+     crash.play();
+     break;
+
+     case "l":
+     var kick= new Audio("sounds/kick-bass.mp3");
+     kick.play();
+     break;
+
+     default:
+    console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey){
+  document.querySelector("."+ currentKey).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+ currentKey).classList.remove("pressed");
+  },100);
+}
